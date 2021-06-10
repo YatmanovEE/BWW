8a4109322017e5e49bcd6bea3308f71963b5ac2a
Yatmanov Evgeniy <64483257+moizhirok@users.noreply.github.com>
2021-06-10 22:30:18 +0300

    Update registryGlobalName function, cause this function can be only registry new global Name

--- src/modules/join.ts
+++ src/modules/join.ts
@@ -37,9 +37,7 @@ export function createClassName(className: ICreateClassName) {
 	};
 }
 
-export function registryGlobalName(
-	className?: ICreateClassName
-): ICreateClassName {
+export function registryGlobalName(className: ICreateClassName): void {
 	if (className) {
 		if (globalName) {
 			console.error('Уже зарегистрирован один глобальный стиль');
@@ -47,5 +45,5 @@ export function registryGlobalName(
 			globalName = className;
 		}
 	}
-	return globalName;
+}
 }
