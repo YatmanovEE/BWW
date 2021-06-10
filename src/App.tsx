eefe9af98aa4cc5e4e9259e1b5e8355023678821
Yatmanov Evgeniy <64483257+moizhirok@users.noreply.github.com>
2021-06-10 22:31:48 +0300

    Added modal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import { registryGlobalName } from './modules/join';
+import { getGlobalName, registryGlobalName } from './modules/join';
 import BlackboardWithWords from './components/BlackboardWithWords';
 import ThemeApp from './components/ThemeApp';
 import { ThemeChangedWrapper } from './components/ThemeChangedWrapper';
 import { globalClassName } from './App.global.className';
+import Modal from './components/Modal';
 
 const App = () => {
 	let className = globalClassName();
 	registryGlobalName(className);
+	let join = getGlobalName();
 	return (
 		<>
 			<ThemeApp>
-				<div className={className.flex}>
-					<ThemeChangedWrapper></ThemeChangedWrapper>
+				<div className={join('flex', 'wrap')}>
 					<BlackboardWithWords></BlackboardWithWords>
 				</div>
 			</ThemeApp>
+			<Modal></Modal>
 		</>
 	);
 };
