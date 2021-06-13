763233c073e95ae5297f02b1c469f07817c037fd
Yatmanov Evgeniy <64483257+moizhirok@users.noreply.github.com>
2021-06-13 21:51:18 +0300

    Added listeners for production

--- server/index.js
+++ server/index.js
@@ -3,10 +3,12 @@ let app = express();
 let config = require('config');
 let path = require('path');
 const PORT = config.get('server.port') ?? 5000;
-app.use(express.static(path.join(__dirname, '../', 'build')));
-app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, '../', 'build', 'index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '../', 'build')));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, '../', 'build', 'index.html'));
+	});
+}
 app.listen(PORT, () => {
 	console.log(`server is started on port ${PORT}`);
 });
