0719b5a60a4ea0ad01b876ad4a81873971f62cbd
Yatmanov Evgeniy <64483257+moizhirok@users.noreply.github.com>
2021-06-13 21:41:19 +0300

    added get handler for any request

--- server/index.js
+++ server/index.js
@@ -3,8 +3,10 @@ let app = express();
 let config = require('config');
 let path = require('path');
 const PORT = config.get('server.port') ?? 5000;
-app.use(express.static(path.join(__dirname, 'build')));
-
+app.use(express.static(path.join(__dirname, '../', 'build')));
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, '../', 'build', 'index.html'));
+});
 app.listen(PORT, () => {
 	console.log(`server is started on port ${PORT}`);
 });
