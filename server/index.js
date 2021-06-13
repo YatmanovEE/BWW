f9a8b9a1af40f236dd04f6b55899ad5205793d1f
Yatmanov Evgeniy <64483257+moizhirok@users.noreply.github.com>
2021-06-13 23:24:23 +0300

    Completely redesigned the project structure

--- server/index.js
+++ server/index.js
@@ -4,9 +4,11 @@ let config = require('config');
 let path = require('path');
 const PORT = config.get('server.port') ?? 5000;
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, '../', 'build')));
+	app.use(express.static(path.join(__dirname, '../', 'client', 'build')));
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, '../', 'build', 'index.html'));
+		res.sendFile(
+			path.resolve(__dirname, '../', 'build', 'client', 'index.html')
+		);
 	});
 }
 app.listen(PORT, () => {
